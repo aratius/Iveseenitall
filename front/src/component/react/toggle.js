1f8456322989ec3bb5b9101f747ae33dd3361ee7
aratius <arata1129matsu@icloud.com>
2021-02-14 00:33:58 +0900

    ローカルでfirestoreにデータセット

--- front/src/component/react/toggle.js
+++ front/src/component/react/toggle.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import Firebase from '../utils/firebase'
 
 export default class Toggle extends React.Component {
 
+  onClick = (e) => {
+    console.log(e.target.checked)
+    Firebase.setData(e.target.checked)
+  }
+  
   render () {
     return (
       <>
         <label htmlFor="" >toggle</label>
-        <input type="checkbox" />
+        <input type="checkbox" onClick={this.onClick}/>
       </>
     )
   }
--- front/src/component/utils/firebase.js
+++ front/src/component/utils/firebase.js
@@ -1,6 +1,25 @@
+import firebase from 'firebase'
+import "firebase/firestore"
+
+const firebaseconfig = {
+}
 
 class _Firebase {
   constructor() {
+    firebase.initializeApp({
+      apiKey: "6119ff6bb29a54e3a8781eb20dd299eaee13d026",
+      projectId: "led-lighting-18db7"
+    })
+
+    this.db = firebase.firestore()
+  }
+  
+  setData(tf) {
+    console.log("setData");
+    this.db.collection("switch").doc("1").update({ isOn: tf }).then(docRef => {
+    }).catch(err => {
+      console.log(err)
+    })
   }
   
 }
